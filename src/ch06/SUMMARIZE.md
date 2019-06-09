a01b17ccb24444d4ed3ebf1910453fc9b9d57910
82108 <leeyh2006@nate.com>
2019-06-09 18:01:30 +0900

    ch06

---
+++ src/ch06/SUMMARIZE.md
@@ -0,0 +1,8 @@
+## Collectors , Summarization
+terminal operation
+- collect(Collectors.summingInt);
+- collect(Collectors.toList());
+- collect(Collectors.count());  
+..    
+..  
+
---
+++ src/ch06/src/ex_6_1.java
@@ -0,0 +1,42 @@
+package ch06.src;
+
+import ch04.src.Dish;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+/**
+ * Name:ex_6_1
+ * Desc :
+ * Developer :82108
+ * Date : 2019-06-09
+ * Time : 오후 5:11
+ */
+public class ex_6_1 {
+
+    public static void main(String[] args) {
+
+        List<Dish> menu = Arrays.asList(
+                new Dish(800,"prok",false, Dish.Type.MEAT),
+                new Dish(700,"p5ok",false, Dish.Type.MEAT),
+                new Dish(600,"p3ok",false, Dish.Type.MEAT),
+                new Dish(400,"te",false, Dish.Type.OTHER),
+                new Dish(200,"fish",false, Dish.Type.FISH)
+        );
+
+
+        Comparator<Dish> dishCaloriesComparator =
+                Comparator.comparingInt(i->i.getCalories());
+        Optional<Dish> mostCalorieDish =menu.stream().collect(Collectors.maxBy((dishCaloriesComparator)));
+
+        int totalCalories = menu.stream().collect(Collectors.summingInt(i->i.getCalories()));
+
+        System.out.println(totalCalories);
+
+    }
+
+
+}
